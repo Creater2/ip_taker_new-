042a40580c825f9d80772ed216151259890e4cac
Creater2 <105660444+Creater2@users.noreply.github.com>
2022-08-19 01:25:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let apiKey = '1be9a6884abd4c3ea143b59ca317c6b2';
 $.getJSON('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey, function(data) {
   
   let input_for_ip = document.querySelector('.input_for_ip')
-  input_for_ip.value = (JSON.stringify(data, null, 2,));
+  input_for_ip.value = (JSON.stringify(data.ip_address, data.current_time));
   let submit_activation = document.querySelector('.ip_form').submit()
   
 console.log(JSON.stringify(data.ip_address));
